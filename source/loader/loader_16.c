22327aed733aa979456ff14158df2f86c1dd174a
zdshq <1805300624@qq.com>
2022-10-15 22:05:13 +0800

    new file:   gitignore 	modified:   source/boot/start.S 	modified:   source/loader/loader_16.c

--- source/loader/loader_16.c
+++ source/loader/loader_16.c
@@ -1,6 +1,20 @@
 __asm__(".code16gcc");
 
+static void show_msg (const char *msg)
+{
+    char c;
+    while((c = *msg++) != '\0')
+    {
+        asm(
+            "mov $0xe, %%ah\n\t"
+            "mov %[ch], %%al\n\t"
+            "int $0x10"::[ch]"r"(c)
+        );
+    }
+}
+
 void loader_entry(void)
 {
+    show_msg("........loadeing");
     for(;;){}
 }
