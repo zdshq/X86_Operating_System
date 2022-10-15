a2c99af80017baf660819d0def4d97f29d8afce1
zdshq <1805300624@qq.com>
2022-10-15 16:00:29 +0800

    first commit

---
+++ build/source/boot/CMakeFiles/boot.dir/boot.c.obj.d
@@ -0,0 +1,3 @@
+source/boot/CMakeFiles/boot.dir/boot.c.obj: \
+ H:/project/x86_operating/diy-x86os/start/start/source/boot/boot.c \
+ H:/project/x86_operating/diy-x86os/start/start/source/boot/boot.h
